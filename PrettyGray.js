0caf8f5bea5f18f6c692fdb5d94721cb21baf381
Snus1x <107406386+Snus1x@users.noreply.github.com>
2023-04-21 14:47:17 +0300

    Update PrettyGray.js

--- PrettyGray.js
+++ PrettyGray.js
@@ -1,1 +1,1 @@
-{"Accent":"#000000","Segment":"#363636","Main":"#0f0f0f","Text":"#ababab","Subtext":"#ffffff","Card":"#292929","BorderRadius":"1","BorderRadiusUnit":"em","Controls":"Side","Presets":"","Margins":"1","MarginsUnit":"em","CustomFontEnabled":false}
+{"Accent":"#000000","Segment":"#0a0a0a","Main":"#101010","Text":"#ababab","Subtext":"#ffffff","Card":"#292929","BorderRadius":"1","BorderRadiusUnit":"em","Controls":"Side","Presets":"","Margins":"1","MarginsUnit":"em","CustomFontEnabled":false}
